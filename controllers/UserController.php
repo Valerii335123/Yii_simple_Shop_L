02c1c6e04af747b5c01eee09f39ff0dd9ca394c3
Valerii <valerasobco@gmail.com>
2020-11-25 18:59:28 +0200

    finall sort name and between price and admin delete user

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -111,6 +111,7 @@ class UserController extends Controller
      */
     public function actionDelete($id)
     {
+
         $this->findModel($id)->delete();
 
         return $this->redirect(['index']);
--- modules/admin/controllers/DefaultController.php
+++ modules/admin/controllers/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace app\modules\admin\controllers;
 
+use app\models\User;
 use yii\web\Controller;
 use Yii;
 /**
@@ -21,15 +22,27 @@ class DefaultController extends Controller
 
 
   }
+
+
+  public function actionDeleteuser($id) {
+      $m=User::find()->where(['id'=>$id])->one();
+      $m->delete();
+     // print_r($m);
+      return $this->render('index');
+  }
     /**
      * Renders the index view for the module
      * @return string
      */
     public function actionIndex()
     {
+        $model=User::find()->all();
+
         //перевірка на адміна працює потрібно для кожної сторіки перевірку не зручно і довго
             if($this->checkAdmin())
-        return $this->render('index');
+        return $this->render('index',[
+            'model'=>$model,
+        ]);
             //стрічка нижче не працює 
             else return  Yii::$app->response->redirect('site/login');
             // можливо використати goBack();
